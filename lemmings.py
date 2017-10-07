85a78c08ed9f8658cb918217ab15973ebd94b657
vagrant <vagrant@vagrant.vm>
2017-10-07 19:45:00 +0000

    finished pseudocoding

--- lemmings.py
+++ lemmings.py
@@ -22,6 +22,16 @@ Find the farthest any single lemming needs to travel for food.
 def furthest(num_holes, cafes):
     """Find longest distance between a hole and a cafe."""
 
+    # convert # of holes into a list range
+        # 3 --> [0, 1, 2]
+
+    # loop through each item in range w/ counter
+    # if current index matches one from cafes list, stop; hold final count value
+    # through other iterations, check if total count > current count
+    # at the end, you'll have the highest value count from all iterations
+    # return that #
+
+
 if __name__ == '__main__':
     import doctest
     if doctest.testmod().failed == 0:
